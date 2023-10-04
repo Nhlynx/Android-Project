bcae7d77161e0f13de532bd4df10833ab94657ce
Nhlynx <dzaki.aufa.itxpro@gmail.com>
2023-10-04 14:15:03 +0700

    4 Oktober 2023 Part 4

--- app/src/main/java/com/example/belajarandroidactivity/DashboardStoreActivity.kt
+++ app/src/main/java/com/example/belajarandroidactivity/DashboardStoreActivity.kt
@@ -2,10 +2,33 @@ package com.example.belajarandroidactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 
 class DashboardStoreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_store)
+
+        var ibMin:ImageButton = findViewById(R.id.ib_min)
+        var ibPlus:ImageButton = findViewById(R.id.ib_plus)
+        var tvCount:TextView = findViewById(R.id.tvCount)
+        var tvTotal:TextView = findViewById(R.id.tvTotalHarga)
+        var btnCheckout:Button = findViewById(R.id.btnCheckout)
+
+        var angka = tvTotal.text.toString().toInt()
+        var harga:Int = 68020668
+
+        ibMin.setOnClickListener {
+            angka-=1
+            tvCount.text = angka.toString()
+            tvTotal.text = (harga*angka).toString()
+        }
+        ibPlus.setOnClickListener {
+            angka+=1
+            tvCount.text = angka.toString()
+            tvTotal.text = (harga*angka).toString()
+        }
     }
 }
